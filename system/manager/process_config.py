bd743eaf5b751b6f212c1acdc7cf7b4c56e30db5
Comma Device <device@comma.ai>
2026-02-20 00:17:52 +0000

    fix(manager): prevent duplicate joystick process spawn
    Disable automatic joystick process in process_config.py to avoid
    manager respawning joystick_control

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -112,7 +112,7 @@ procs = [
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
   PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
   PythonProcess("webjoystick", "tools.bodyteleop.web", notcar),
-  PythonProcess("joystick", "tools.joystick.joystick_control", and_(joystick, iscar)),
+  #PythonProcess("joystick", "tools.joystick.joystick_control", and_(joystick, iscar)),
 ]
 
 managed_processes = {p.name: p for p in procs}
