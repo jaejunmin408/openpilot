588f925f1503ee3da8c2b570e2e4208d411ca059
leegw1211 <leegw1211@gmail.com>
2026-03-19 16:28:48 +0900

    임시 : logmessaged로 인한 engage 불가 문제를 해결하기 위해 무시하도록 설정

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -267,6 +267,7 @@ class SelfdriveD:
     num_events = len(self.events)
 
     not_running = {p.name for p in self.sm['managerState'].processes if not p.running and p.shouldBeRunning}
+    not_running -= {'logmessaged'}  # 임시: logmessaged 무시
     if self.sm.recv_frame['managerState'] and len(not_running):
       if not_running != self.not_running_prev:
         cloudlog.event("process_not_running", not_running=not_running, error=True)
